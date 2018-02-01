GNOME/geary

commit 178ce35113c7408d7ca8b3353f430040dda84fb3

Fix MailboxAddresses.to_rfc822_string formatting, add unit tests.

src/engine/rfc822/rfc822-mailbox-addresses.vala
@@ -105,15 +105,16 @@ public class Geary.RFC822.MailboxAddresses : Geary.MessageData.AbstractMessageDa
     }
 
     /**
-     * Returns the addresses suitable for insertion into an RFC822 message.  RFC822 quoting is
-     * performed if required.
+     * Returns the addresses suitable for insertion into an RFC822 message.
+     *
+     * RFC822 quoting is performed if required.
      *
      * @see MailboxAddress.to_rfc822_string
      */
     public string to_rfc822_string() {
-        return MailboxAddress.list_to_string(addrs, "", (a) => a.to_rfc822_string());
+        return MailboxAddress.list_to_string(addrs, ", ", (a) => a.to_rfc822_string());
     }
-    
+
     public uint hash() {
         // create sorted set to ensure ordering no matter the list's order
         Gee.TreeSet<string> sorted_addresses = traverse<RFC822.MailboxAddress>(addrs)
@@ -143,14 +144,14 @@ public class Geary.RFC822.MailboxAddresses : Geary.MessageData.AbstractMessageDa
         
         return Collection.are_sets_equal<RFC822.MailboxAddress>(first, second);
     }
-    
+
     /**
      * See Geary.MessageData.SearchableMessageData.
      */
     public string to_searchable_string() {
-        return MailboxAddress.list_to_string(addrs, "", (a) => a.to_searchable_string());
+        return MailboxAddress.list_to_string(addrs, " ", (a) => a.to_searchable_string());
     }
-    
+
     public override string to_string() {
         return MailboxAddress.list_to_string(addrs, "(no addresses)", (a) => a.to_string());
     }

test/engine/rfc822-mailbox-addresses-test.vala
@@ -1,0 +1,46 @@
+/*
+ * Copyright 2018 Michael Gratton <mike@vee.net>
+ *
+ * This software is licensed under the GNU Lesser General Public License
+ * (version 2.1 or later). See the COPYING file in this distribution.
+ */
+
+class Geary.RFC822.MailboxAddressesTest : Gee.TestCase {
+
+    public MailboxAddressesTest() {
+        base("Geary.RFC822.MailboxAddressesTest");
+        add_test("from_rfc822_string_encoded", from_rfc822_string_encoded);
+        add_test("to_rfc822_string", to_rfc822_string);
+    }
+
+    public void from_rfc822_string_encoded() {
+        MailboxAddresses addrs = new MailboxAddresses.from_rfc822_string("test@example.com");
+        assert(addrs.size == 1);
+
+        addrs = new MailboxAddresses.from_rfc822_string("test1@example.com, test2@example.com");
+        assert(addrs.size == 2);
+
+        // Courtesy Mailsploit https://www.mailsploit.com
+        addrs = new MailboxAddresses.from_rfc822_string("\"=?utf-8?b?dGVzdCIgPHBvdHVzQHdoaXRlaG91c2UuZ292Pg==?==?utf-8?Q?=00=0A?=\" <demo@mailsploit.com>");
+        assert(addrs.size == 1);
+
+        // Courtesy Mailsploit https://www.mailsploit.com
+        addrs = new MailboxAddresses.from_rfc822_string("\"=?utf-8?Q?=42=45=47=49=4E=20=2F=20=28=7C=29=7C=3C=7C=3E=7C=40=7C=2C=7C=3B=7C=3A=7C=5C=7C=22=7C=2F=7C=5B=7C=5D=7C=3F=7C=2E=7C=3D=20=2F=20=00=20=50=41=53=53=45=44=20=4E=55=4C=4C=20=42=59=54=45=20=2F=20=0D=0A=20=50=41=53=53=45=44=20=43=52=4C=46=20=2F=20?==?utf-8?b?RU5E=?=\",        <demo@mailsploit.com>");
+        assert(addrs.size == 2);
+    }
+
+    public void to_rfc822_string() {
+        assert(new_addreses({ "test1@example.com" }).to_rfc822_string() ==
+               "test1@example.com");
+        assert(new_addreses({ "test1@example.com", "test2@example.com" }).to_rfc822_string() ==
+               "test1@example.com, test2@example.com");
+    }
+
+    private MailboxAddresses new_addreses(string[] address_strings) {
+        Gee.List<MailboxAddress> addresses = new Gee.LinkedList<MailboxAddress>();
+        foreach (string address in address_strings) {
+            addresses.add(new MailboxAddress(null, address));
+        }
+        return new MailboxAddresses(addresses);
+    }
+}

test/CMakeLists.txt
@@ -20,6 +20,7 @@ set(TEST_ENGINE_SRC
   engine/imap-engine/account-processor-test.vala
   engine/mime-content-type-test.vala
   engine/rfc822-mailbox-address-test.vala
+  engine/rfc822-mailbox-addresses-test.vala
   engine/rfc822-message-test.vala
   engine/rfc822-message-data-test.vala
   engine/rfc822-utils-test.vala

test/meson.build
@@ -16,6 +16,7 @@ geary_test_engine_sources = [
   'engine/imap-engine/account-processor-test.vala',
   'engine/mime-content-type-test.vala',
   'engine/rfc822-mailbox-address-test.vala',
+  'engine/rfc822-mailbox-addresses-test.vala',
   'engine/rfc822-message-test.vala',
   'engine/rfc822-message-data-test.vala',
   'engine/rfc822-utils-test.vala',

test/test-engine.vala
@@ -37,6 +37,7 @@ int main(string[] args) {
     engine.add_suite(new Geary.JS.Test().get_suite());
     engine.add_suite(new Geary.Mime.ContentTypeTest().get_suite());
     engine.add_suite(new Geary.RFC822.MailboxAddressTest().get_suite());
+    engine.add_suite(new Geary.RFC822.MailboxAddressesTest().get_suite());
     engine.add_suite(new Geary.RFC822.MessageTest().get_suite());
     engine.add_suite(new Geary.RFC822.MessageDataTest().get_suite());
     engine.add_suite(new Geary.RFC822.Utils.Test().get_suite());

commit 7073206c8d03890e69f4c29f26ae7da4df2e979f

Fix client use of MailboxAddress(es).to_rfc822_string().
Turns out the client was in inexplicably using
MailboxAddresses.to_rfc822_string() in a bunch of places to display
emails to users, mostly in the composer. This fixes that.

Followup to b7eea857.

* src/engine/rfc822/rfc822-mailbox-addresses.vala (MailboxAddresses):
  Make list param for default ctor nullable for your testing convenience,
  add a test. Add to_full_display() method that the client should nearly
  always be using to display a list to users, use that in all the places.
  Remove empty param for list_to_string since all but one callers just
  pass in the empty string, which also fixes some stupid regressions
  introduced in commit 178ce351 that causes it actually break
  to_rfc822_string, rather than fix it as claimed, update call sites.

src/client/composer/composer-widget.vala
@@ -2011,7 +2011,7 @@ public class ComposerWidget : Gtk.EventBox {
     private bool add_account_emails_to_from_list(Geary.Account other_account, bool set_active = false) {
         Geary.RFC822.MailboxAddresses primary_address = new Geary.RFC822.MailboxAddresses.single(
             other_account.information.primary_mailbox);
-        this.from_multiple.append_text(primary_address.to_rfc822_string());
+        this.from_multiple.append_text(primary_address.to_full_display());
         this.from_list.add(new FromAddressMap(other_account, primary_address));
         if (!set_active && this.from.equal_to(primary_address)) {
             this.from_multiple.set_active(this.from_list.size - 1);
@@ -2026,7 +2026,10 @@ public class ComposerWidget : Gtk.EventBox {
                 // Displayed in the From dropdown to indicate an "alternate email address"
                 // for an account.  The first printf argument will be the alternate email
                 // address, and the second will be the account's primary email address.
-                string display = _("%1$s via %2$s").printf(addresses.to_rfc822_string(), other_account.information.display_name);
+                string display = _("%1$s via %2$s").printf(
+                    addresses.to_full_display(),
+                    other_account.information.display_name
+                );
                 this.from_multiple.append_text(display);
                 this.from_list.add(new FromAddressMap(other_account, addresses));
                 

src/client/composer/contact-entry-completion.vala
@@ -63,10 +63,10 @@ public class ContactEntryCompletion : Gtk.EntryCompletion {
         Gtk.CellRendererText text_renderer = (Gtk.CellRendererText) cell;
         text_renderer.markup = highlighted_result;
     }
-    
+
     private bool on_match_selected(Gtk.EntryCompletion sender, Gtk.TreeModel model, Gtk.TreeIter iter) {
-        string full_address = list_store.get_rfc822_string(iter);
-        
+        string full_address = list_store.to_full_address(iter);
+
         Gtk.Entry? entry = sender.get_entry() as Gtk.Entry;
         if (entry == null)
             return false;

src/client/composer/contact-list-store.vala
@@ -104,9 +104,9 @@ public class ContactListStore : Gtk.ListStore {
         
         return (Geary.Contact) contact_value.get_object();
     }
-    
-    public string get_rfc822_string(Gtk.TreeIter iter) {
-        return get_contact(iter).get_rfc822_address().to_rfc822_string();
+
+    public string to_full_address(Gtk.TreeIter iter) {
+        return get_contact(iter).get_rfc822_address().to_full_display();
     }
 
     private inline void add_contact(Geary.Contact contact) {

src/client/composer/email-entry.vala
@@ -32,7 +32,7 @@ public class EmailEntry : Gtk.Entry {
             
             updating = true;
             modified = true;
-            text = (addresses == null) ? "" : addresses.to_rfc822_string();
+            text = (addresses == null) ? "" : addresses.to_full_display();
             updating = false;
         });
 

src/engine/imap-db/imap-db-message-row.vala
@@ -262,27 +262,7 @@ private class Geary.ImapDB.MessageRow {
     }
 
     private static string? flatten_addresses(RFC822.MailboxAddresses? addrs) {
-        if (addrs == null)
-            return null;
-        
-        switch (addrs.size) {
-            case 0:
-                return null;
-            
-            case 1:
-                return addrs[0].to_rfc822_string();
-            
-            default:
-                StringBuilder builder = new StringBuilder();
-                foreach (RFC822.MailboxAddress addr in addrs) {
-                    if (!String.is_empty(builder.str))
-                        builder.append(", ");
-                    
-                    builder.append(addr.to_rfc822_string());
-                }
-                
-                return builder.str;
-        }
+        return (addrs == null || addrs.size == 0) ? null : addrs.to_rfc822_string();
     }
 
     private RFC822.MailboxAddress? unflatten_address(string? str) {

src/engine/rfc822/rfc822-mailbox-addresses.vala
@@ -27,11 +27,10 @@ public class Geary.RFC822.MailboxAddresses :
      * returned.
      */
     private static string list_to_string(Gee.List<MailboxAddress> addrs,
-                                          string empty,
                                           ListToStringDelegate to_s) {
         switch (addrs.size) {
             case 0:
-                return empty;
+                return "";
 
             case 1:
                 return to_s(addrs[0]);
@@ -61,8 +60,10 @@ public class Geary.RFC822.MailboxAddresses :
     private Gee.List<MailboxAddress> addrs = new Gee.ArrayList<MailboxAddress>();
 
 
-    public MailboxAddresses(Gee.Collection<MailboxAddress> addrs) {
-        this.addrs.add_all(addrs);
+    public MailboxAddresses(Gee.Collection<MailboxAddress>? addrs = null) {
+        if (addrs != null) {
+            this.addrs.add_all(addrs);
+        }
     }
 
     public MailboxAddresses.single(MailboxAddress addr) {
@@ -147,6 +148,17 @@ public class Geary.RFC822.MailboxAddresses :
     }
 
     /**
+     * Returns the addresses suitable for display to a human.
+     *
+     * @return a string containing each message in the list,
+     * serialised by a call to {@link Message.to_full_display},
+     * separated by commas.
+     */
+    public string to_full_display() {
+        return list_to_string(addrs, (a) => a.to_full_display());
+    }
+
+    /**
      * Returns the addresses suitable for insertion into an RFC822 message.
      *
      * RFC822 quoting is performed if required.
@@ -154,7 +166,7 @@ public class Geary.RFC822.MailboxAddresses :
      * @see MailboxAddress.to_rfc822_string
      */
     public string to_rfc822_string() {
-        return list_to_string(addrs, ", ", (a) => a.to_rfc822_string());
+        return list_to_string(addrs, (a) => a.to_rfc822_string());
     }
 
     public uint hash() {
@@ -191,11 +203,13 @@ public class Geary.RFC822.MailboxAddresses :
      * See Geary.MessageData.SearchableMessageData.
      */
     public string to_searchable_string() {
-        return list_to_string(addrs, " ", (a) => a.to_searchable_string());
+        return list_to_string(addrs, (a) => a.to_searchable_string());
     }
 
     public override string to_string() {
-        return list_to_string(addrs, "(no addresses)", (a) => a.to_string());
+        return this.size > 0
+            ? list_to_string(addrs, (a) => a.to_string())
+            : "(no addresses)";
     }
 
 }

test/engine/rfc822-mailbox-addresses-test.vala
@@ -30,10 +30,11 @@ class Geary.RFC822.MailboxAddressesTest : Gee.TestCase {
     }
 
     public void to_rfc822_string() {
-        assert(new_addreses({ "test1@example.com" }).to_rfc822_string() ==
-               "test1@example.com");
-        assert(new_addreses({ "test1@example.com", "test2@example.com" }).to_rfc822_string() ==
-               "test1@example.com, test2@example.com");
+        assert(new MailboxAddresses().to_rfc822_string() == "");
+        assert(new_addreses({ "test1@example.com" })
+               .to_rfc822_string() == "test1@example.com");
+        assert(new_addreses({ "test1@example.com", "test2@example.com" })
+               .to_rfc822_string() == "test1@example.com, test2@example.com");
     }
 
     private MailboxAddresses new_addreses(string[] address_strings) {