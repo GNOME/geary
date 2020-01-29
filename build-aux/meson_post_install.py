0efcc48e41652a036ef32dbafab36828946a5c89
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2020-01-29 15:28:28 +0100

    Remove unneded gschema

--- build-aux/meson_post_install.py
+++ build-aux/meson_post_install.py
@@ -8,7 +8,5 @@ if not environ.get('DESTDIR', ''):
     DATA_DIR = path.join(PREFIX, 'share')
     print('Updating icon cache...')
     call(['gtk-update-icon-cache', '-qtf', path.join(DATA_DIR, 'icons/hicolor')])
-    print("Compiling new schemas...")
-    call(["glib-compile-schemas", path.join(DATA_DIR, 'glib-2.0/schemas')])
     print("Updating desktop database...")
     call(["update-desktop-database", path.join(DATA_DIR, 'applications')])
