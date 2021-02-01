GNOME/geary

commit e8061379ec022f8ce1ecb163a574192af5b8ac80

Application.Controller: Clean up config/data vars in ctor

src/client/application/application-controller.vala
@@ -125,6 +125,9 @@ internal class Application.Controller :
         this.application = application;
         this.controller_open = cancellable;
 
+        GLib.File config_dir = application.get_user_config_directory();
+        GLib.File data_dir = application.get_user_data_directory();
+
         // This initializes the IconFactory, important to do before
         // the actions are created (as they refer to some of Geary's
         // custom icons)
@@ -139,9 +142,7 @@ internal class Application.Controller :
             this.application.get_web_extensions_dir(),
             this.application.get_user_cache_directory().get_child("web-resources")
         );
-        Components.WebView.load_resources(
-            this.application.get_user_config_directory()
-        );
+        Components.WebView.load_resources(config_dir);
         Composer.WebView.load_resources();
         ConversationWebView.load_resources();
         Accounts.SignatureWebView.load_resources();
@@ -179,7 +180,7 @@ internal class Application.Controller :
         // Hook up cert, accounts and credentials machinery
 
         this.certificate_manager = yield new Application.CertificateManager(
-            this.application.get_user_data_directory().get_child("pinned-certs"),
+            config_dir.get_child("pinned-certs"),
             cancellable
         );
 
@@ -189,8 +190,8 @@ internal class Application.Controller :
 
         this.account_manager = new Accounts.Manager(
             libsecret,
-            this.application.get_user_config_directory(),
-            this.application.get_user_data_directory()
+            config_dir,
+            data_dir
         );
         this.account_manager.account_added.connect(
             on_account_added

commit c4fae24262c6e6763649085dd021b1c5cf5dcd53

Application.Controller: Fix directory used for locally pinned certs
Fix defect introduced in commit e8061379.

src/client/application/application-controller.vala
@@ -189,8 +189,14 @@ internal class Application.Controller :
         // Hook up cert, accounts and credentials machinery
 
         this.certificate_manager = yield new Application.CertificateManager(
-            config_dir.get_child("pinned-certs"),
+            data_dir.get_child("pinned-certs"),
             cancellable
+        );
+        // Commit e8061379 mistakenly used config_dir for cert manager
+        // above, so remove it if found. This can be pulled out post
+        // v40.
+        Geary.Files.recursive_delete_async(
+            config_dir.get_child("pinned-certs")
         );
 
         SecretMediator? libsecret = yield new SecretMediator(cancellable);